5f55655e7d9c2d2fa8debcca2ad88e3b45aa75ed
Vasvari Botond <89163562+Dejavu333@users.noreply.github.com>
2023-07-17 02:21:57 +0200

    Add files via upload

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class BotyRenamer
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(429, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "útvonal";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 159);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(511, 282);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(429, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "átnevez";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(334, 47);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(189, 29);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Válassza ki, miket szeretne átnevezni :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(221, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Adja meg a számozandó nevet :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Hajtsa végre a változtatást :";
+            // 
+            // BotyRenamer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(535, 453);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "BotyRenamer";
+            this.Text = "BotyWare Renamer";
+            this.Load += new System.EventHandler(this.VisualizeNN_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class BotyRenamer : Form
+    {
+        public BotyRenamer()
+        {
+            InitializeComponent();       
+        }
+
+        private void VisualizeNN_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        //variables
+        string[] source; string[] destination; string replacement = "";
+
+        //BUTTON1 
+        private void button1_Click(object sender, EventArgs e)
+        {       
+
+            OpenFileDialog v1 = new OpenFileDialog();
+            v1.Multiselect = true;
+            v1.Title = "BotyRenamer";
+            v1.ShowDialog();
+
+            richTextBox1.Text = "";
+            source = new string[v1.FileNames.Length];
+            destination = new string[v1.FileNames.Length];
+
+            int counter = 0;
+            foreach (String file in v1.FileNames)
+            {
+                richTextBox1.Text = richTextBox1.Text + " " + file;
+
+                source[counter] = file;
+                counter=counter+1;         
+            }
+
+        }//button1 function ends
+
+        //BUTTON2
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+             replacement = richTextBox2.Text;
+
+            if (replacement.Length>0 && source !=null) 
+            {
+                for (int i = 0; i < source.Length; i++)
+                {      
+                    string actual = source[i];
+                    string result = "";
+
+                    result = Regex.Replace(actual, @"[\w.]+\.", replacement+$"{i + 1}"+".");    //richTextBox2.Text +"számozás" az legyen a destination tömb minden egyes elemének az utolsó \ és a .kiterjesztés közötti része
+                    destination[i] = result;
+                    try
+                    {
+                        File.Move(source[i], destination[i]);
+                        richTextBox1.Text = "Sikeres művelet";
+                    }
+                    catch (Exception)
+                    {
+                        richTextBox1.Text = "Az újabb művelethez válasszon ismét fájlokat!";
+                    }         
+                }             
+            }
+
+            else if (replacement.Length == 0) 
+            {
+                richTextBox1.Text = "Adjon meg nevet!";
+            }
+
+            else if (source == null)
+            {
+                richTextBox1.Text = "Válasszon ki fáljokat!";
+            }
+
+        }//button2 function ends
+
+    }
+
+}
